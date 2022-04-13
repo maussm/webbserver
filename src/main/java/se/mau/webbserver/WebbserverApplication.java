a4dce3fce88d404c5d87df75947aa946c2d6bdd0
Emile <emile@ecspresso.com>
2022-04-13 12:42:20 +0200

    Spring initializer

---
+++ src/main/java/se/mau/webbserver/WebbserverApplication.java
@@ -0,0 +1,13 @@
+package se.mau.webbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebbserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/mau/webbserver/WebbserverApplicationTests.java
@@ -0,0 +1,13 @@
+package se.mau.webbserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
