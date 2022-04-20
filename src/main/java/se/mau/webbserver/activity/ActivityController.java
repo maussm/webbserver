be17699322f086a85961aa03b18b7b20620fae18
vince <vincentparikwestlund@gmail.com>
2022-04-20 13:07:50 +0200

    Modeled Cost Center according to DB

--- src/main/java/se/mau/webbserver/activity/ActivityController.java
+++ src/main/java/se/mau/webbserver/activity/ActivityController.java
@@ -1,12 +1,13 @@
 package se.mau.webbserver.activity;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping(path = "/test")
 public class ActivityController {
     private final ActivityService activityService;
