7dba39c7e9a14583d41162746e2906e5497f0aed
Jens Bjerre <jensebjerre@gmail.com>
2022-04-20 13:29:45 +0200

    Participant fleshed out

--- src/main/java/se/mau/webbserver/participant/Participant.java
+++ src/main/java/se/mau/webbserver/participant/Participant.java
@@ -1,4 +1,42 @@
 package se.mau.webbserver.participant;
 
+import javax.persistence.*;
+
+@Entity
+@Table
 public class Participant {
+    @Id
+    @SequenceGenerator(
+            name = "participent_seq",
+            sequenceName = "participent_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "participent_seq"
+    )
+    private Long id;
+    private String name;
+    public Participant(){}
+
+    public Participant(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
