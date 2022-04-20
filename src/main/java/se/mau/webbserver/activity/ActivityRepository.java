a114b6ae51ad7e6685b08fa5f9650f5ee6f48de2
Emile <emile@ecspresso.com>
2022-04-20 13:53:10 +0200

    Mer testdata, get/post/delete aktivitet, exponera fel, söka på aktivitet
    Lagt till testdata.
    
    Nya mappningar:
    GET /activity/{id}
    POST /new/activity + body
    DELETE /delete/activity/{activityId}
    
    Söka efter aktivit på aktivity id.
    
    Visa felmeddelande till klienten.

--- src/main/java/se/mau/webbserver/activity/ActivityRepository.java
+++ src/main/java/se/mau/webbserver/activity/ActivityRepository.java
@@ -2,6 +2,9 @@ package se.mau.webbserver.activity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
-public interface ActivityRepository extends JpaRepository<Activity, Long> { }
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+    Optional<Activity> findActivityByActivityId(String activityId);
+}
