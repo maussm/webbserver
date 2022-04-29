099b6a1cc4e948a11fca8c8c174352d0a690a28a
Emile <emile@ecspresso.com>
2022-04-29 09:39:28 +0200

    Ändrade till nonnull på metod och parameter enligt interface

--- src/main/java/se/mau/webbserver/restapi/costcenter/CostCenterRepository.java
+++ src/main/java/se/mau/webbserver/restapi/costcenter/CostCenterRepository.java
@@ -1,10 +1,11 @@
 package se.mau.webbserver.restapi.costcenter;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNull;
 import java.util.Optional;
 
 public interface CostCenterRepository extends JpaRepository<CostCenter, Long> {
     @Override
-    Optional<CostCenter> findById( Long costCenterId);
+    @NonNull
+    Optional<CostCenter> findById(@NonNull Long costCenterId);
 }
