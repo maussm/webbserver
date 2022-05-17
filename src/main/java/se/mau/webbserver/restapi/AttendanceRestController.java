1c5f001a0ed925a2e934771b1e69661bd49b5786
Jens Bjerre <jensebjerre@gmail.com>
2022-05-17 15:49:51 +0200

    Update AttendanceRestController.java
    added dto fix in for attendence

--- src/main/java/se/mau/webbserver/restapi/AttendanceRestController.java
+++ src/main/java/se/mau/webbserver/restapi/AttendanceRestController.java
@@ -10,7 +10,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.mau.webbserver.entity.attendance.Attendance;
+import se.mau.webbserver.entity.attendance.AttendanceDTO;
 import se.mau.webbserver.entity.attendance.AttendanceService;
+import se.mau.webbserver.entity.cost_center.CostCenter;
+import se.mau.webbserver.entity.cost_center.CostCenterService;
+import se.mau.webbserver.entity.participant.Participant;
+import se.mau.webbserver.entity.participant.ParticipantService;
+
 import java.util.List;
 
 @RestController
@@ -18,10 +24,14 @@ import java.util.List;
 public class AttendanceRestController {
 
     private final AttendanceService attendanceService;
+    private final ParticipantService participantService;
+    private final CostCenterService costCenterService;
 
     @Autowired
-    public AttendanceRestController(AttendanceService attendanceService) {
+    public AttendanceRestController(AttendanceService attendanceService, ParticipantService participantService, CostCenterService costCenterService) {
         this.attendanceService = attendanceService;
+        this.participantService = participantService;
+        this.costCenterService = costCenterService;
     }
 
     @GetMapping
@@ -34,8 +44,17 @@ public class AttendanceRestController {
     }
 
     @PostMapping
-    public void addAttendance(@RequestBody Attendance attendance) {
+    public void addAttendance(@RequestBody AttendanceDTO attendanceDTO) {
+        Participant participant = participantService.getParticipant(attendanceDTO.getParticipant_id());
+        CostCenter costCenter = costCenterService.getCostCenter(attendanceDTO.getCost_center_id());
+
+        Attendance attendance = new Attendance();
+        attendance.setParticipant(participant);
+        attendance.setC(costCenter);
+        attendance.setDate(attendanceDTO.getDate());
+
         attendanceService.addAttendance(attendance);
+
     }
 
     @DeleteMapping("/{id}")
